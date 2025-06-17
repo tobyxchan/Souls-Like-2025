b2278dd17a8ecb54e4b2668c9524d676269ea778
tobyxchan <137049136+tobyxchan@users.noreply.github.com>
2025-06-18 09:11:37 +1000

    Player Movement and Rotation

---
+++ Assets/Scripts/Player/Movement/InputManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerControls playerControls;
+
+    public Vector2 movementInput;
+    public float verticalInput, horizontalInput;
+
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+
+            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+        }
+
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
+    }
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizontalInput = movementInput.x;
+    }
+}
