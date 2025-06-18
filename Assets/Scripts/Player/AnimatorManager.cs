4533fba48fa19881ffe92cbe66d5372a2ab6e512
tobyxchan <137049136+tobyxchan@users.noreply.github.com>
2025-06-18 12:00:48 +1000

    Basic walking and running animations

--- Assets/Scripts/Player/AnimatorManager.cs
+++ Assets/Scripts/Player/AnimatorManager.cs
@@ -7,6 +7,9 @@ public class AnimatorManager : MonoBehaviour
     Animator animator;
     int horizontal, vertical;
 
+    // Animation Snapping Variables
+    
+
 
     private void Awake()
     {
@@ -18,7 +21,56 @@ public class AnimatorManager : MonoBehaviour
 
     public void UpdateAnimatorValues(float horizontalMovement, float verticalMovement)
     {
-        animator.SetFloat(horizontal, horizontalMovement, 0.1f, Time.deltaTime);
-        animator.SetFloat(vertical, verticalMovement, 0.1f, Time.deltaTime);
+        float snappedHorizontal;
+        float snappedVertical;
+
+        // Animation Snapping (OPTIONAL)
+        #region Snapped Horizontal
+        if (horizontalMovement > 0 && horizontalMovement < 0.55f)
+        {
+            snappedHorizontal = 0.5f;
+        }
+        else if (horizontalMovement > 0.55f)
+        {
+            snappedHorizontal = 1;
+        }
+        else if (horizontalMovement < 0 && horizontalMovement > -0.55f)
+        {
+            snappedHorizontal = -0.5f;
+        }
+        else if (horizontalMovement < -0.55f)
+        {
+            snappedHorizontal = -1;
+        }
+        else
+        {
+            snappedHorizontal = 0;
+        }
+        #endregion
+        #region Snapped Vertical
+        if (verticalMovement > 0 && verticalMovement < 0.55f)
+        {
+            snappedVertical = 0.5f;
+        }
+        else if (verticalMovement > 0.55f)
+        {
+            snappedVertical = 1f;
+        }
+        else if (verticalMovement < 0 && verticalMovement > -0.55f)
+        {
+            snappedVertical = -0.5f;
+        }
+        else if (verticalMovement < -0.55f)
+        {
+            snappedVertical = -1f;
+        }
+        else
+        {
+            snappedVertical = 0;
+        }
+        #endregion
+
+        animator.SetFloat(horizontal, snappedHorizontal, 0.1f, Time.deltaTime);
+        animator.SetFloat(vertical, snappedVertical, 0.1f, Time.deltaTime);
     }
 }
--- Assets/Scripts/Player/Movement/InputManager.cs
+++ Assets/Scripts/Player/Movement/InputManager.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     PlayerControls playerControls;
+    AnimatorManager animatorManager;
 
     public Vector2 movementInput;
+    private float moveAmount;
     public float verticalInput, horizontalInput;
 
+
+    private void Awake()
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+    }
 
     private void OnEnable()
     {
@@ -36,5 +43,7 @@ public class InputManager : MonoBehaviour
     {
         verticalInput = movementInput.y;
         horizontalInput = movementInput.x;
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animatorManager.UpdateAnimatorValues(0, moveAmount);
     }
 }
